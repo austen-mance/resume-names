57bcc6baa3ac5f8667ea894c3b6c3ca584d55fb9
Lord Amber <imabluesman@gmail.com>
2016-05-05 01:56:52 -0500

    datasets and scraping scripts

--- scraper/dataset_cleaner.py
+++ scraper/dataset_cleaner.py
@@ -2,35 +2,40 @@ import pandas as pd
 
 # drop duplicates
 
-df = pd.DataFrame.from_csv('dataset.csv')
+df = pd.DataFrame.from_csv('dataset.csv', index_col='original_link')
 df.drop_duplicates(subset="link")
 
 # setup what will become the 6 new datasets
 
 chicago_sales = pd.DataFrame()
 chicago_retail = pd.DataFrame()
-boston_sales = pd.DataFrame()
-boston_retail = pd.DataFrame()
+nyc_sales = pd.DataFrame()
+nyc_retail = pd.DataFrame()
 la_sales = pd.DataFrame()
 la_retail = pd.DataFrame()
 
-datasets = {'chicago_sales_dataset.csv': chicago_sales, 'chicago_retail_dataset.csv': chicago_retail, 'boston_sales_dataset.csv': boston_sales, 'boston_retail_dataset.csv': boston_retail, 'la_sales_dataset.csv': la_sales, 'la_retail_dataset.csv': la_retail}
+# change links
+
+# df.link = df.link.apply(lambda x: x.split('?',1)[0])
+
+# create zip code lists for LA, boston
+# check accounts
+# edit resumes
 
 # now we break up our df
 # into the 6 datasets by
 # location and job type
 
-sales_df = df[df.type == 'Sales']
-retail_df = df[df.link == 'Retail']
-
-chicago_sales =sales_df[sales_df.city == 'Chicago']
-boston_sales =sales_df[sales_df.city == 'Boston']
-la_sales =sales_df[sales_df.city == 'Los Angeles']
-chicago_retail =retail_df[retail_df.city == 'Chicago']
-boston_retail =retail_df[retail_df.location.city == 'Boston']
-la_retail =retail_df[retail_df.location.city == 'Los Angeles']
+chicago_sales = df[(df.city == 'Chicago') & (df.type == 'Sales')]
+nyc_sales = df[(df.city == 'New York') & (df.type == 'Sales')]
+la_sales = df[(df.city == 'Los Angeles') & (df.type == 'Sales')]
+chicago_retail = df[(df.city == 'Chicago') & (df.city == 'Retail')]
+nyc_retail = df[(df.city == 'New York') & (df.type == 'Retail')]
+la_retail = df[(df.city == 'Los Angeles') & (df.type == 'Retail')]
 
 # save the files as csv
 
-for data in datasets:
+datasets = {'chicago_sales_dataset.csv': chicago_sales, 'chicago_retail_dataset.csv': chicago_retail, 'nyc_sales_dataset.csv': nyc_sales, 'nyc_retail_dataset.csv': nyc_retail, 'la_sales_dataset.csv': la_sales, 'la_retail_dataset.csv': la_retail}
+
+for data in datasets.keys():
 	datasets[data].to_csv(data)
